4f4007d0c988a9e210135c3f5f5d516ef470f490
David Vaca Sánchez <david.vacasanchez@gmail.com>
2015-06-17 18:54:17 +0100

    Añadido Modelo I (PostgreSQL)

--- models/models.js
+++ models/models.js
@@ -2,8 +2,27 @@ var path = require('path');
 // Cargamos el Modelo ORM
 var Sequelize = require('sequelize');
 
-// Usar la BBDD Sqlite
-var bbdd = new Sequelize(null, null, null, {dialect: "sqlite", storage:"quiz.sqlite"});
+//DATABASE_URL = postgres://user:passwd@host:port/database
+//DATABASE_STORAGE = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6] || null);
+var user = (url[2] || null);
+var pwd = (url[3] || null);
+var protocol = (url[1] || null);
+var dialect = (url[1] || null);
+var port = (url[5] || null);
+var host = (url[4] || null);
+var storage = process.env.DATABASE_STORAGE;
+
+// Usar la BBDD Sqlite o PostgreSQL
+var bbdd = new Sequelize(DB_name, user, pwd, {
+	dialect: dialect,
+	protocol: protocol,
+	port: port,
+	host: host,
+	storage: storage, //Solo Sqlite
+	omitNull: true    //Solo Postgre
+});
 
 // Importar la definición de la tabla Quiz en quiz.js
 var Quiz = bbdd.import(path.join(__dirname, 'quiz'));

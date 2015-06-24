7551e2fe76c5c87850848fccd70902e8c39c2ac0
David Vaca Sánchez <david.vacasanchez@gmail.com>
2015-06-24 11:00:47 +0100

    Olvido al actualizar

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -118,6 +118,7 @@ exports.update = function(req, res)
 {
 	req.quiz.pregunta = req.body.quiz.pregunta;
 	req.quiz.respuesta = req.body.quiz.respuesta;
+	req.quiz.CategoryId = req.body.quiz.CategoryId;
 	
 	req.quiz.validate().then(function(err){
 		if(err)
